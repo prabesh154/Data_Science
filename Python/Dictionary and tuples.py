b49867b9e72f7d20ede3900ad0ed5d3df35941bd
Prabesh Rijal <rijalprabesh154@gmail.com>
2022-03-28 18:14:56 +0545

    Change the Directory

---
+++ Python/Dictionary and tuples.py
@@ -0,0 +1,9 @@
+d={"tom":1232,"robin:":2434,"prabesh":23423}
+
+for key in d:
+    print("key:",key,"value:",d[key])
+
+print("tom" in d)
+# gives True
+# Tuples has heterogenous values where as list has similar values
+# Tuples are inmutable
---
+++ Python/continue and for.py
@@ -0,0 +1,17 @@
+'''keylocation ="chair"
+locations=["bed","kitchen","closet","chair"]
+
+for i in locations:
+    if i==keylocation:
+      print("Key is found in:",i)
+      break
+    else :
+       print("key is not found")'
+'''
+
+'''//continue statement
+for i in range(1,6):
+    if i%2==0:
+        continue
+    print(i*i)
+'''
